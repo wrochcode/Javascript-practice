ffbac1029a3341f38b199931926feaacf4316cc3
Wahyu Rochman <wrochcode@gmail.com>
2022-09-09 05:21:14 +0700

    Create app-3.js

---
+++ parsinta-javascript-basic/app-3.js
@@ -0,0 +1,12 @@
+const firstname = 'Wahyu';
+const lastname = 'Rochman';
+
+// const fullname = ['Wahyu', 'Rochman']; //salah
+// const fullname = ['Wahyu', 'Rochman'].join(' '); //benar
+
+// sama
+// const fullname = 'My name is ' + firstname + ' ' + lastname;
+const fullname = `My name is ${firstname} ${lastname}`;
+
+console.log(fullname);
+// console.log(fullname);
