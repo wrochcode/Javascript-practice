0aa97cfee7a9161500ef0d0808e657492a401178
Wahyu Rochman <wrochcode@gmail.com>
2022-09-10 14:41:08 +0700

    Update app-11.js

--- parsinta-javascript-basic/app-11.js
+++ parsinta-javascript-basic/app-11.js
@@ -64,5 +64,39 @@ const people = [
 // console.log(mapPeople); 
 
 //cara 3
-const findPerson = people.find((item) => item.age<=30); // menampilkan 1 data/objek yang memenuhi kondisi tersebut
-console.log(findPerson); 
+// const findPerson = people.find((item) => item.age<=30); // menampilkan 1 data/objek yang memenuhi kondisi tersebut
+// console.log(findPerson); 
+
+//cara 4
+// people.forEach((item)=> console.log(item.firstname));
+// people.forEach((item)=> console.log(`First name is ${item.firstname}`));
+
+//ngide
+// let number = 1;
+// let totalData = people.length;
+// people.forEach((item)=>{
+//     console.log(`First name in array ${number} is ${item.firstname}`);
+//     if(number<totalData){
+//         number++;
+//     }
+// });
+// console.log(`last number is ${number}`);
+
+//cara 5
+// const somePeople = people.some((item)=>item.age>=35);//mengecek satu persatu dengan output true or false
+// console.log(somePeople);
+
+//cara 6
+// const everyPeople = people.every((item)=>item.age>=35);//memberikan kesimpulan array dengan kondisi yang menghasilkan output true or false
+// console.log(everyPeople);
+
+//cara 7
+// const reducePeople = people.reduce((currentItem,item)=>{ //fungsi menghitung
+// return item.age + currentItem;
+// }, 0);
+// console.log(reducePeople);
+
+// cara 8
+const number = [1, 2, 3, 4, 5, 6];
+let x =number.includes(3);//mengecek apakah terdapat bilangan dan berlaku hanya untuk simple array/ array 1 dimensi
+console.log(x);
