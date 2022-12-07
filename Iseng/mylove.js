2a05a3ff120400b12cca62c5ceb85a3ca19ea97f
Wahyu Rochman <wrochcode@gmail.com>
2022-12-07 13:06:43 +0700

    Create mylove.js

---
+++ Iseng/mylove.js
@@ -0,0 +1,12 @@
+/* Jika cowo programmer berzodiak cancer
+sedang suka sama cewe */
+
+const my_love = 69;
+let day = 1;
+
+while(my_love > 0){
+    if(my_love !=0){
+        console.log(`I'll try the best for u. Forever and ever`);
+        day++;
+    }
+}
